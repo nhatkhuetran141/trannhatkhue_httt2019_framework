c46d0485862fa8f515ad971e2cbde1d54454882c
nhatkhue141 <nhatkhuetran142@gmail.com>
2021-09-13 20:40:33 +0700

    avh

--- demo/Program.cs
+++ demo/Program.cs
@@ -6,7 +6,7 @@ namespace demo
     {
         static void Main(string[] args)
         {
-            //demo hello World
+            Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
         }
     }
