782274f66c159554c13b77404e937bbe16c0e953
nhatkhue141 <nhatkhuetran142@gmail.com>
2021-09-13 21:59:50 +0700

    Hello World

--- demo/Program.cs
+++ demo/Program.cs
@@ -5,9 +5,23 @@ namespace demo
     class Program
     {
         static void Main(string[] args)
+        {           
+            Console.WriteLine("Enter number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Number is: " + n);
+            if (n == 1)
+            {
+                basic();
+            }
+            else if (n == 2)
+            {
+                Console.WriteLine("hi");
+            }
+        }
+        static void basic()
         {
-           
             Console.WriteLine("Hello World!");
         }
+
     }
 }
